c0d8fdf053ba76da87659e1f54980573e39080d0
Jakoc Rivas <jrivas@intellisys.com.do>
2019-07-29 15:09:21 -0400

    adds an author to  package.json file

--- example.test.js
+++ example.test.js
@@ -1,23 +1,25 @@
+test();
+
 // test("nothing", () => {
 //   console.log("hola");
 // });
 
-{
-  describe("Google", () => {
-    beforeAll(async () => {
-      await page.goto("https://www.google.com/");
-    });
+// {
+//   describe("Google", () => {
+//     beforeAll(async () => {
+//       await page.goto("https://www.google.com/");
+//     });
 
-    it('should display "google" text on page', async () => {
-      await expect(page).toMatch("google");
-    });
+//     it('should display "google" text on page', async () => {
+//       await expect(page).toMatch("google");
+//     });
 
-    it("takes photo", async () => {
-      // const browser = await puppeteer.launch({headless: false});
-      // const page = await browser.newPage();
-      // await page.goto('https://google.com');
-      await page.setViewport({ width: 1000, height: 500 });
-      await page.screenshot({ path: "google.png" });
-    });
-  });
-}
+//     it("takes photo", async () => {
+//       // const browser = await puppeteer.launch({headless: false});
+//       // const page = await browser.newPage();
+//       // await page.goto('https://google.com');
+//       await page.setViewport({ width: 1000, height: 500 });
+//       await page.screenshot({ path: "google.png" });
+//     });
+//   });
+// }
