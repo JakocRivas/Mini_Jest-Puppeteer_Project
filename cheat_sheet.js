f00b24771e26ba344f30ba5e2b65f22692351c3b
Jakoc Rivas <jrivas@intellisys.com.do>
2019-07-29 11:29:46 -0400

    setting enviroment

--- cheat_sheet.js
+++ cheat_sheet.js
@@ -18,6 +18,7 @@ browser.close();
 
 //soemthing like this
 import puppeteer from "puppeteer";
+import { DH_CHECK_P_NOT_PRIME } from "constants";
 test("h1 loads correctly", async () => {
   let browser = await puppeteer.launch();
   let page = await browser.newPage();
@@ -39,3 +40,77 @@ test("h1 loads correctly", async () => {
 // Utilize enviroment variables
 
 // Utilize headless
+
+//Getting Content
+
+//page.$eval === document.querySelector
+//page.$$eval === Array.from(document.querySelectorAll(Selector))
+
+//interacting
+
+//page.click(selector)
+//page.tap(selector)
+// page.type(selector,text)
+
+//something like this
+const lastNameEL = await page.$('[data-testid="lastName"]');
+const emaildEL = await page.$('[data-testid="email"]');
+const passwordEL = await page.$('[data-testid="password"]');
+const submitEL = await page.$('[data-testid="submit"]');
+
+await firstNameEL.tap();
+
+await page.type('[data-testid="firstName"]', user.firstName);
+
+await lastNameEL.tap();
+
+await page.type('[data-testid="lastName"]', user.lastName);
+
+await emaildEL.tap();
+
+await page.type('[data-testid="email"]', user.email);
+
+await passwordEL.tap();
+
+await page.type('[data-testid="password"]', user.password);
+
+await submitEL.tap();
+
+//intercepting requests
+page.setRequetsInterception();
+interceptedRequest.url.includes("");
+interceptedRequest.abort();
+
+//emulating mobile
+page.emulate({
+  viewport: { options },
+  userAgent: string
+});
+
+//capturing console msgs
+page.on("console");
+page.on("pageerror");
+
+//JWTs?
+page.setCookie({});
+
+//visual Regression Tests
+// page.screenshot(); + differing Library (pixelmatch)
+
+//performance test
+//unused CSS
+//unused JS
+//page weight
+await page._clinet.send('performance.enabble')
+
+const perf = await.page.evaluate(_ => ({
+    firstPaint:
+        performance.loadTimes().firstPaintTime * 1000 -
+        performance.timing.navigationStart
+}))
+
+
+
+
+//PUPPETEER RECORDER
+//chrome extension
