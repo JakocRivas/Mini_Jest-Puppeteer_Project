84d3b966fd673113d15b3c0160d5d72b46c87da7
Jakoc Rivas <jrivas@intellisys.com.do>
2019-07-29 14:33:43 -0400

    add jest preset in package.json file

--- example.test.js
+++ example.test.js
@@ -1,3 +1,23 @@
-test("nothing", () => {
-  console.log("hola");
-});
+// test("nothing", () => {
+//   console.log("hola");
+// });
+
+{
+  describe("Google", () => {
+    beforeAll(async () => {
+      await page.goto("https://www.google.com/");
+    });
+
+    it('should display "google" text on page', async () => {
+      await expect(page).toMatch("google");
+    });
+
+    it("takes photo", async () => {
+      // const browser = await puppeteer.launch({headless: false});
+      // const page = await browser.newPage();
+      // await page.goto('https://google.com');
+      await page.setViewport({ width: 1000, height: 500 });
+      await page.screenshot({ path: "google.png" });
+    });
+  });
+}
