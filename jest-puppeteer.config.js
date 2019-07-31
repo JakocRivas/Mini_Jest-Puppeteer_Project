b12f873bb471d9e12a01c5dcc1cc21b006ea1c11
Jakoc Rivas <jrivas@intellisys.com.do>
2019-07-31 13:14:26 -0400

    adds a eslint configuration file and adds configuration to jest-puppeter.configuration and adds a branch "letoyo" to test changes

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   launch: {
-    headless: false
-  }
+    headless: false,
+    slowMo: 250
+  },
+  globalSetup: "jest-environment-puppeteer/setup",
+  globalTeardown: "jest-environment-puppeteer/teardown",
+  testEnvironment: "jest-environment-puppeteer",
+  setupTestFrameworkScriptFile: "expect-puppeteer"
 };
