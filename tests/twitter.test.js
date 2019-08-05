74929735d00c6f63cff8b486290b41e68c6b9076
Jakoc Rivas <jrivas@intellisys.com.do>
2019-08-05 11:18:32 -0400

    creates a test in "twiitter.test.js" to go to twitters homepage and checks that the h1 of the page loads

--- tests/twitter.test.js
+++ tests/twitter.test.js
@@ -1,24 +1,39 @@
-describe("Twitter", () => {
+describe("Basic", () => {
   beforeAll(async () => {
     await page.goto("https://twitter.com/");
   });
 
-  it('should display "twitter" text on page', async () => {
-    await expect(page).toMatch("twitter");
+  it('should display "See what’s happening in the world right now" text on page', async () => {
+    const h1 = await page.$eval(
+      "#doc > div > div.StaticLoggedOutHomePage-content > div.StaticLoggedOutHomePage-cell.StaticLoggedOutHomePage-utilityBlock > div.StaticLoggedOutHomePage-signupBlock > h1",
+      text => text.textContent
+    );
+    // const text = await page.evaluate(() => document.body.textContent);
+    expect(h1).toBe("See what’s happening in the world right now");
   });
 });
 
-it("should open a new page", async () => {
-  let page = await browser.newPage();
-  await page.goto("https://google.com");
-});
+// describe("Twitter", () => {
+//   beforeAll(async () => {
+//     await page.goto("https://twitter.com/");
+//   });
 
-it("should open a new page", async () => {
-  // const page = await browser.newPage();
-  await page.goto("https://google.com");
-});
+//   it('should display "twitter" text on page', async () => {
+//     await expect(page).toMatch("twitter");
+//   });
+// });
 
-it("should fill an input", async () => {
-  await page.waitForSelector(".gLFyf");
-  await page.type(".gLFyf", "Hello");
-});
+// it("should open a new page", async () => {
+//   let page = await browser.newPage();
+//   await page.goto("https://google.com");
+// });
+
+// it("should open a new page", async () => {
+//   // const page = await browser.newPage();
+//   await page.goto("https://google.com");
+// });
+
+// it("should fill an input", async () => {
+//   await page.waitForSelector(".gLFyf");
+//   await page.type(".gLFyf", "Hello");
+// });
