2297f1ee164b69eb404ef73e77fedeb23a24f79d
Jakoc Rivas <jrivas@intellisys.com.do>
2019-08-05 10:04:53 -0400

    creates a "page object" folder

--- tests/twitter.test.js
+++ tests/twitter.test.js
@@ -1,17 +1,17 @@
-// describe("Twitter", () => {
-//   beforeAll(async () => {
-//     await page.goto("https://twitter.com/");
-//   });
+describe("Twitter", () => {
+  beforeAll(async () => {
+    await page.goto("https://twitter.com/");
+  });
 
-//   test('should display "twitter" text on page', async () => {
-//     await expect(page).toMatch("twitter");
-//   });
-// });
+  it('should display "twitter" text on page', async () => {
+    await expect(page).toMatch("twitter");
+  });
+});
 
-// it("should open a new page", async () => {
-//   let page = await browser.newPage();
-//   await page.goto("https://google.com");
-// });
+it("should open a new page", async () => {
+  let page = await browser.newPage();
+  await page.goto("https://google.com");
+});
 
 it("should open a new page", async () => {
   // const page = await browser.newPage();
