7dd9337cd331e54f50dad742725f25f1e24c829c
Jakoc Rivas <jrivas@intellisys.com.do>
2019-08-06 08:47:11 -0400

    adds a "log in" test

--- tests/twitter.test.js
+++ tests/twitter.test.js
@@ -19,6 +19,7 @@ describe("Twitter", () => {
 
     const emailField = ".js-username-field.email-input.js-initial-focus";
     const passwordField = ".js-password-field";
+
     const submitButton =
       ".submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium";
 
@@ -34,9 +35,23 @@ describe("Twitter", () => {
     await page.type(emailField, email);
     await page.type(passwordField, password);
 
-    // await page.waitForSelector(submitButton);
+    await page.waitForSelector(submitButton);
     await page.click(submitButton);
+
+    const home =
+      "#react-root > div > div > div > main > div > div.css-1dbjc4n.r-aqfbo4.r-1niwhzg.r-16y2uox > div > div.css-1dbjc4n.r-14lw9ot.r-1tlfku8.r-1ljd8xs.r-13l2t4g.r-1phboty.r-1jgb5lz.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div > div.css-1dbjc4n.r-aqfbo4.r-14lw9ot.r-my5ep6.r-rull8r.r-qklmqi.r-gtdqiz.r-ipm5af.r-1g40b8q > div.css-1dbjc4n.r-1loqt21.r-136ojw6 > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci > div > h2 > span";
+    await page.waitForSelector(home);
+
+    const timeline = await page.evaluate(home => {
+      return document.querySelector(home).textContent;
+    }, home);
+
+    expect(timeline).toBe("Home");
   });
+
+  // test("creates post", async () => {
+  //   const
+  // })
 });
 
 // describe("Twitter", () => {
