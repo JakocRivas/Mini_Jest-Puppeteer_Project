7823f2842f750fb281b99ba902c32d0bd4e8ab22
Jakoc Rivas <jrivas@intellisys.com.do>
2019-08-22 10:36:54 -0400

    creates a folder PO and more files to store page objects
    creates a homepage,logingpage and profile page to further improve the implamantation of the page object model

--- tests/twitterOld.test.js
+++ tests/twitterOld.test.js
@@ -1,12 +1,12 @@
-const header = require("../PageObjects/Twitter/LoginTwitter/h1");
-const loginFields = require("../PageObjects/Twitter/LoginTwitter/loginForm");
-const loginFormButton = require("../PageObjects/Twitter/LoginTwitter/submitButton");
-const submitButton = require("../PageObjects/Twitter/LoginTwitter/loginButton");
-const commentBox = require("../PageObjects/Twitter/CommentTwitter/commentBox");
-const timeline = require("../PageObjects/Twitter/CommentTwitter/timeline");
-const post = require("../PageObjects/Twitter/CommentTwitter/post");
-const search = require("../PageObjects/Twitter/SearchTwitter/search");
-const profile = require("../PageObjects/Twitter/ProfileTwitter/profile");
+const header = require("../PageObjects/Twitter/PO/LoginTwitter/h1");
+const loginFields = require("../PageObjects/Twitter/PO/LoginTwitter/loginForm");
+const loginFormButton = require("../PageObjects/Twitter/PO/LoginTwitter/submitButton");
+const submitButton = require("../PageObjects/Twitter/PO/LoginTwitter/loginButton");
+const commentBox = require("../PageObjects/Twitter/PO/CommentTwitter/commentBox");
+const timeline = require("../PageObjects/Twitter/PO/CommentTwitter/timeline");
+const post = require("../PageObjects/Twitter/PO/CommentTwitter/post");
+const search = require("../PageObjects/Twitter/PO/SearchTwitter/search");
+const profile = require("../PageObjects/Twitter/PO/ProfileTwitter/profile");
 const common = require("../resources/common");
 
 // const testAtrr = (something, attr) => {
