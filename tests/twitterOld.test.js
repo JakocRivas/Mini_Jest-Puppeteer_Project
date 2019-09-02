2c6d5e7cfe1f37c94d156453a83dd989ac6b9214
Jakoc Rivas <jrivas@intellisys.com.do>
2019-09-02 13:01:37 -0400

    deletes all the unused imports

--- tests/twitterOld.test.js
+++ tests/twitterOld.test.js
@@ -1,13 +1,3 @@
-const header = require("../PageObjects/Twitter/PO/LoginTwitter/h1");
-const loginFields = require("../PageObjects/Twitter/PO/LoginTwitter/loginForm");
-const loginFormButton = require("../PageObjects/Twitter/PO/LoginTwitter/submitButton");
-const submitButton = require("../PageObjects/Twitter/PO/LoginTwitter/loginButton");
-const commentBox = require("../PageObjects/Twitter/PO/CommentTwitter/commentBox");
-const timeline = require("../PageObjects/Twitter/PO/CommentTwitter/timeline");
-const post = require("../PageObjects/Twitter/PO/CommentTwitter/post");
-const search = require("../PageObjects/Twitter/PO/SearchTwitter/search");
-const profile = require("../PageObjects/Twitter/PO/ProfileTwitter/profile");
-const common = require("../resources/common");
 const LoginPage = require("../PageObjects/Twitter/loginPage");
 const HomePage = require("../PageObjects/Twitter/homepage");
 const ProfilePage = require("../PageObjects/Twitter/profilePage");
@@ -17,23 +7,12 @@ const ProfilePage = require("../PageObjects/Twitter/profilePage");
 //   return wrapper;
 // };
 
-function makeid(length) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
 describe("Twitter", () => {
   beforeAll(async () => {
     await page.goto("https://twitter.com/");
     await page.setViewport({ width: 1366, height: 768 });
   });
-  beforeEach(async () => {
+  beforeAll(async () => {
     loginPage = new LoginPage();
     homePage = new HomePage();
     profilePage = new ProfilePage();
@@ -68,7 +47,7 @@ describe("Twitter", () => {
   });
 
   //takes a screenshot of the element
-  xit("should download profile image", async () => {
+  it("should download profile image", async () => {
     await profilePage.sayCheese();
   });
 });
